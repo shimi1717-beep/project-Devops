890ae9b78cbbb2c8361ada7abbb38fecfc3a2c05
shimi1717 <shimi1717@gmail.com>
2026-01-02 10:25:48 +0200

    Add /version endpoint

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ def hello():
 def health():
     return jsonify(status="ok"), 200
 
+
+@app.get("/version")
+def version():
+    return jsonify(version="1.0.0", author="shimi"), 200
+
 if __name__ == "__main__":
     # Bind to 0.0.0.0 so it works inside Docker
     app.run(host="0.0.0.0", port=80)
